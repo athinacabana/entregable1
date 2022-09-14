108d937d8e0c0780c4e3364584b45d89f283c7eb
athinacabana <athina.cabana@cardenalspinola.com.ar>
2022-09-14 02:48:10 -0300

    segundo commit

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express")
 const app = express()
 const path = require("path")
-app.use(express.static("public"))
+
+
 app.listen(3030, () => console.log("servidor corriendo en 3030"))
+
+app.use(express.static(path.join(__dirname, 'public')))
