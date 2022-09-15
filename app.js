78054d2b317ba43d32d0ffee3275a8dbd2caa24d
athinacabana <athina.cabana@cardenalspinola.com.ar>
2022-09-14 21:09:10 -0300

    tercer commit

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const app = express()
 const path = require("path")
 
 
-app.listen(3030, () => console.log("servidor corriendo en 3030"))
+app.use(express.static("public"))
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.get("/",(req,res) => {
+    res.sendFile(path.resolve("./views/home.html"))
+})
+
+app.listen(3000, () => {
+    console.log ('Servidor corriendo')
+})
