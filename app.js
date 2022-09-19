e019cdc7346e6aa444f67f5c23eebddccb4de9fe
athinacabana <athina.cabana@cardenalspinola.com.ar>
2022-09-19 15:40:14 -0300

    actualizacion

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-const express = require("express")
-const app = express()
-const path = require("path")
+const express = require ('express');
 
+const app = express ();
 
-app.use(express.static("public"))
+const path = require ('path');
 
-app.get("/",(req,res) => {
-    res.sendFile(path.resolve("./views/home.html"))
-})
+const publicpath = path.resolve (__dirname,'./publics')
 
-app.listen(3000, () => {
+app.use(express.static(publicpath));
+
+app.listen (3000, ()=>{
     console.log ('Servidor corriendo')
-})
+});
+
+app.get ('/', (req,res)=>{
+    res.sendFile (path.join(__dirname, 'views/index.html'))
+});
